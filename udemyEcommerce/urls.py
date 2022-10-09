dddf0d93c2ccaaeca67c9f0b5ed9b8ccd714b4fd
Kaan Senol <kaansenol@gmail.com>
2022-10-09 16:12:23 +0200

    category slugs added to url.py

--- udemyEcommerce/urls.py
+++ udemyEcommerce/urls.py
@@ -26,5 +26,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("store.html", include("store.urls")),
+    path("store.html/", include("store.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
