daf41a6a717b4a99984741955967c76a212becf9
Kaan Senol <kaansenol@gmail.com>
2022-10-13 15:08:20 +0200

    nav bar and filter categories & product detail page

--- category/models.py
+++ category/models.py
@@ -1,5 +1,7 @@
+from audioop import reverse
 from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,6 +14,10 @@ class Category(models.Model):
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+    def get_url(self):
+        return reverse("products_by_category", args=[self.slug])
+
 
     def __str__(self):
         return self.category_name
