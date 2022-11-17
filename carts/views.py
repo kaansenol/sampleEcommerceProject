9e62adbfc3e20db486fec82f076b211c1cc88cf8
Kaan Senol <kaansenol@gmail.com>
2022-11-17 15:40:48 +0100

    Adding different variations of one item in cart

--- carts/views.py
+++ carts/views.py
@@ -39,12 +39,12 @@ def add_cart(request, product_id):
     cart.save()
 
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.create(product=product, quantity = 1, cart=cart)
         if len(product_variation) > 0:
             cart_item.variations.clear()
             for i in product_variation:
                 cart_item.variations.add(i)
-        cart_item.quantity += 1
+        #cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
